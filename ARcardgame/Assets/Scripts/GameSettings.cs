876a81e313a7ebe76a6014e5301f7b6fc6aedc4a
zogak <zogak7@gmail.com>
2021-04-08 20:51:34 +0900

    UI include

---
+++ ARcardgame/Assets/Scripts/GameSettings.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//scene 전체에서 쓰일 게임 세팅 static 변수 저장
+public class GameSettings
+{
+    public static string playerName;
+    public static string roomCode;
+}
